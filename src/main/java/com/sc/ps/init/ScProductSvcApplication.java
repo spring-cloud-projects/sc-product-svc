d0d071fc2a61b93680990ef85bc652129f81799c
Arnab Mondal <arnab2807@gmail.com>
2019-03-20 11:43:36 +0530

    Add files via upload

---
+++ src/main/java/com/sc/ps/init/ScProductSvcApplication.java
@@ -0,0 +1,13 @@
+package com.sc.ps.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScProductSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScProductSvcApplication.class, args);
+	}
+
+}
